cbba21b2f09abce43355b757a44d42a31d0c3d6d
Candelaria <96026527+candecolombresg@users.noreply.github.com>
2022-08-12 01:25:13 -0300

    Update myApp.js
    solution

--- myApp.js
+++ myApp.js
@@ -1,48 +1,34 @@
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet');
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
+const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
+/*To improve performance, most browsers prefetch DNS records for the links in a page. 
+In that way the destination ip is already known when the user clicks on a link. 
+This may lead to over-use of the DNS service, privacy issues, or page statistics alteration 
+(some links may appear visited even if they are not). If you have high security needs you can 
+disable DNS prefetching, at the cost of a performance penalty.*/
+app.use(helmet.dnsPrefetchControl());
 
+/*If you are releasing an update for your website, and you want the users to always download 
+the newer version, you can (try to) disable caching on client’s browser. It can be useful in 
+development too. Caching has performance benefits, which you will lose, so only use this option 
+when there is a real need.*/
+app.use(helmet.noCache());
 
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }}));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*app.use(helmet()) will automatically include all the middleware introduced above, except noCache(),
+and contentSecurityPolicy(), but these can be enabled if necessary.*/
+app.use(helmet());
 
 
 
